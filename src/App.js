1147c546bc3631cfa71898348a45d2affd0cbd76
roc102 <mdfahadrahman1969@gmail.com>
2024-02-04 11:42:45 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useGoogleLogin } from "@react-oauth/google";
 
-function App() {
+const App = () => {
+  const googleLogin = useGoogleLogin({
+    onSuccess: async (tokenResponse) => {
+      console.log(tokenResponse);
+
+      const userInfoEndpoint = "https://www.googleapis.com/oauth2/v3/userinfo";
+      const userInfoResponse = await fetch(userInfoEndpoint, {
+        headers: {
+          Authorization: `Bearer ${tokenResponse.access_token}`,
+        },
+      });
+
+      if (userInfoResponse.ok) {
+        const userInfo = await userInfoResponse.json();
+        console.log(userInfo);
+      } else {
+        console.error("Failed to fetch user information");
+      }
+    },
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="flex items-center justify-center h-screen">
+        <div className="google-auth">
+          <button
+            onClick={() => {
+              googleLogin();
+            }}
+            className="border border-gray-300 hover:bg-gray-200 text-gray-800 py-2 px-4 rounded-full w-full mb-4 flex items-center justify-center"
+          >
+            <span className="mr-2">
+              <img
+                src="https://in.bmscdn.com/webin/common/icons/googlelogo.svg"
+                alt="Google Logo"
+                className="w-6 h-6"
+              />
+            </span>
+            Continue with Google
+          </button>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
