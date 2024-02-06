902ec6ed87062ea5274bf4e6b95f4ef54061b894
roc102 <mdfahadrahman1969@gmail.com>
2024-02-06 15:23:09 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,47 +1,12 @@
-import { useGoogleLogin } from "@react-oauth/google";
+import React from "react";
+import ThemeComponent from "./Theme/ThemeComponent";
+import ThemeProvider from "./Theme/ThemeContext";
 
 const App = () => {
-  const googleLogin = useGoogleLogin({
-    onSuccess: async (tokenResponse) => {
-      console.log(tokenResponse);
-
-      const userInfoEndpoint = "https://www.googleapis.com/oauth2/v3/userinfo";
-      const userInfoResponse = await fetch(userInfoEndpoint, {
-        headers: {
-          Authorization: `Bearer ${tokenResponse.access_token}`,
-        },
-      });
-
-      if (userInfoResponse.ok) {
-        const userInfo = await userInfoResponse.json();
-        console.log(userInfo);
-      } else {
-        console.error("Failed to fetch user information");
-      }
-    },
-  });
   return (
-    <div>
-      <div className="flex items-center justify-center h-screen">
-        <div className="google-auth">
-          <button
-            onClick={() => {
-              googleLogin();
-            }}
-            className="border border-gray-300 hover:bg-gray-200 text-gray-800 py-2 px-4 rounded-full w-full mb-4 flex items-center justify-center"
-          >
-            <span className="mr-2">
-              <img
-                src="https://in.bmscdn.com/webin/common/icons/googlelogo.svg"
-                alt="Google Logo"
-                className="w-6 h-6"
-              />
-            </span>
-            Continue with Google
-          </button>
-        </div>
-      </div>
-    </div>
+    <ThemeProvider>
+      <ThemeComponent />
+    </ThemeProvider>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId="ENTER YOUR CLIENT ID">
-      <App />
-    </GoogleOAuthProvider>
+    <App />
   </React.StrictMode>
 );
